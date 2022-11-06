15fc61e6529987750c4354aeffc8ac67b18bb539
Рустам <rustam19871203@gmail.com>
2022-11-06 22:30:35 +0400

    Первый коммит. Теоретическая часть. Задача.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+    Main.squaring();
 
     }
+    public static void squaring () {
+        int number = 10;
+        int squaringNumber;
+        squaringNumber = number * number;
+        System.out.println("Число " + number + " возведенное в квадрат будет: " + squaringNumber);
+    }
+
 }
+
